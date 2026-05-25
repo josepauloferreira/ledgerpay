a413afd7d03f1cc02275ef3e3ad01ebb05f97eea
Jose P. Ferreira <shzexyz@gmail.com>
2026-05-25 12:07:41 +0000

    test(money): specify creation from two-decimal amount

---
+++ src/test/java/io/github/phlmth/ledgerpay/domain/money/MoneyTest.java
@@ -0,0 +1,18 @@
+package io.github.phlmth.ledgerpay.domain.money;
+
+import java.math.BigDecimal;
+
+import org.junit.jupiter.api.Test;
+import static org.assertj.core.api.Assertions.assertThat;
+
+class MoneyTest {
+    @Test
+    void shouldCreateMoneyWithTwoDecimalPlaces() {
+        var expectedAmount = new BigDecimal("10.50");
+
+        Money money = Money.of("10.50");
+
+        assertThat(money.amount()).isEqualTo(expectedAmount);
+    }
+
+}
