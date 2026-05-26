8f59c732571bb45cb9e4859fcb52729e1f4020c2
Jose P. Ferreira <shzexyz@gmail.com>
2026-05-26 14:48:15 +0000

    test(money): specify rejection of fractional cent amounts

--- src/test/java/io/github/phlmth/ledgerpay/domain/money/MoneyTest.java
+++ src/test/java/io/github/phlmth/ledgerpay/domain/money/MoneyTest.java
@@ -4,40 +4,41 @@ import java.math.BigDecimal;
 
 import org.junit.jupiter.api.Test;
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class MoneyTest {
     @Test
     void shouldCreateMoneyWithTwoDecimalPlaces() {
-        var expectedAmount = new BigDecimal("9.50");
+        var expectedAmount = new BigDecimal("10.50");
 
-        Money money = Money.of("9.50");
+        Money money = Money.of("10.50");
 
         assertThat(money.amount()).isEqualTo(expectedAmount);
     }
 
     @Test
     void shouldNormalizeValueWithoutDecimalPlaces() {
-        var expectedAmount = new BigDecimal("9.00");
+        var expectedAmount = new BigDecimal("10.00");
 
-        Money money = Money.of("9");
+        Money money = Money.of("10");
 
         assertThat(money.amount()).isEqualTo(expectedAmount);
     }
 
     @Test
     void shouldNormalizeAmountCreatedThroughCanonicalConstructor() {
-        var expectedAmount = new BigDecimal("9.00");
+        var expectedAmount = new BigDecimal("10.00");
 
-        Money money = new Money(new BigDecimal("9"));
+        Money money = new Money(new BigDecimal("10"));
 
         assertThat(money.amount()).isEqualTo(expectedAmount);
     }
 
     @Test
     void shouldNormalizeValueWithOneDecimalPlace() {
-        var expectedAmount = new BigDecimal("9.50");
+        var expectedAmount = new BigDecimal("10.50");
 
-        Money money = Money.of("9.5");
+        Money money = Money.of("10.5");
 
         assertThat(money.amount()).isEqualTo(expectedAmount);
     }
@@ -51,5 +52,15 @@ class MoneyTest {
         assertThat(money.amount()).isEqualTo(expectedAmount);
     }
 
+    @Test
+    void shouldRejectFractionalCentAmountFromTextualFactory() {
+        assertThatThrownBy(() -> Money.of("10.001"))
+            .isInstanceOf(IllegalArgumentException.class);
+    }
 
+    @Test
+    void shouldRejectFractionalCentAmountFromCanonicalConstructor() {
+        assertThatThrownBy(() -> new Money(new BigDecimal("10.001")))
+            .isInstanceOf(IllegalArgumentException.class);
+    }
 }
