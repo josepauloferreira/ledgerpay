5ab03e795e784f2f100e645cacfbe8ae236e5111
Jose P. Ferreira <shzexyz@gmail.com>
2026-05-28 16:32:18 +0000

    test(funding): specify rejection of zero amount

--- src/test/java/io/github/phlmth/ledgerpay/domain/funding/TreasuryFundingTest.java
+++ src/test/java/io/github/phlmth/ledgerpay/domain/funding/TreasuryFundingTest.java
@@ -1,6 +1,7 @@
 package io.github.phlmth.ledgerpay.domain.funding;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import io.github.phlmth.ledgerpay.domain.money.Money;
 import io.github.phlmth.ledgerpay.domain.wallet.Wallet;
@@ -19,5 +20,19 @@ class TreasuryFundingTest {
 
     assertThat(treasury.balance()).isEqualTo(Money.of("999900.00"));
     assertThat(destination.balance()).isEqualTo(Money.of("100.00"));
+  }
+
+  @Test
+  void shouldRejectZeroFundingWithoutChangingBalances() {
+    Wallet treasury = new Wallet(Money.of("1000000.00"));
+    Wallet destination = new Wallet(Money.of("0.00"));
+    Money amount = Money.of("0.00");
+    TreasuryFunding funding = new TreasuryFunding();
+
+    assertThatThrownBy(() -> funding.execute(treasury, destination, amount))
+        .isInstanceOf(IllegalArgumentException.class);
+
+    assertThat(treasury.balance()).isEqualTo(Money.of("1000000.00"));
+    assertThat(destination.balance()).isEqualTo(Money.of("0.00"));
   }
 }
