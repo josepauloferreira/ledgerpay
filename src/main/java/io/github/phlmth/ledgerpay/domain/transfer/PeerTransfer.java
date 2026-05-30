47182cfded528c6029334077f01b55ab839a11af
Jose P. Ferreira <shzexyz@gmail.com>
2026-05-30 18:04:00 -0300

    feat(transfer): transfer money between wallets

---
+++ src/main/java/io/github/phlmth/ledgerpay/domain/transfer/PeerTransfer.java
@@ -0,0 +1,12 @@
+package io.github.phlmth.ledgerpay.domain.transfer;
+
+import io.github.phlmth.ledgerpay.domain.money.Money;
+import io.github.phlmth.ledgerpay.domain.wallet.Wallet;
+
+public class PeerTransfer {
+
+  public void execute(Wallet source, Wallet destination, Money amount) {
+    source.debit(amount);
+    destination.credit(amount);
+  }
+}
