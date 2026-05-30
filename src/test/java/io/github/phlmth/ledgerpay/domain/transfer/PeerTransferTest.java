ddbc77efb7d0587c6cf591e1852c8f0324a4de6f
Jose P. Ferreira <shzexyz@gmail.com>
2026-05-30 19:10:06 -0300

    test(transfer): specify rejection of transfer to the same wallet

--- src/test/java/io/github/phlmth/ledgerpay/domain/transfer/PeerTransferTest.java
+++ src/test/java/io/github/phlmth/ledgerpay/domain/transfer/PeerTransferTest.java
@@ -1,6 +1,7 @@
 package io.github.phlmth.ledgerpay.domain.transfer;
 
-import static org.assertj.core.api.Assertions.*;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import io.github.phlmth.ledgerpay.domain.money.Money;
 import io.github.phlmth.ledgerpay.domain.wallet.Wallet;
@@ -61,5 +62,19 @@ class PeerTransferTest {
 
     assertThat(source.balance()).isEqualTo(Money.of("100.00"));
     assertThat(destination.balance()).isEqualTo(Money.of("20.00"));
+  }
+
+  @Test
+  void shouldRejectTransferFromSameWallet() {
+    Wallet source = new Wallet(Money.of("100.00"));
+    Wallet destination = source;
+
+    PeerTransfer transfer = new PeerTransfer();
+
+    assertThatThrownBy(() -> transfer.execute(source, destination, Money.of("40.00")))
+        .isInstanceOf(IllegalArgumentException.class);
+
+    assertThat(source.balance()).isEqualTo(Money.of("100.00"));
+    assertThat(destination.balance()).isEqualTo(Money.of("100.00"));
   }
 }
