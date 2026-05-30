b90efaea33aaeddfeb184cd559267d79b155f0b7
Jose P. Ferreira <shzexyz@gmail.com>
2026-05-30 19:10:55 -0300

    feat(transfer): reject transfer to the same wallet

--- src/main/java/io/github/phlmth/ledgerpay/domain/transfer/PeerTransfer.java
+++ src/main/java/io/github/phlmth/ledgerpay/domain/transfer/PeerTransfer.java
@@ -6,6 +6,10 @@ import io.github.phlmth.ledgerpay.domain.wallet.Wallet;
 public class PeerTransfer {
 
   public void execute(Wallet source, Wallet destination, Money amount) {
+    if (source == destination) {
+      throw new IllegalArgumentException();
+    }
+
     source.debit(amount);
     destination.credit(amount);
   }
